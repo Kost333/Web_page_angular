1afe27562dd66d67179349c698c7076f58dce1dc
Levon <levonkostanyan2021@gmail.com>
2023-07-28 11:08:54 +0400

    create anglar web page

---
+++ src/app/service/data-handler.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Category} from "../model/Category";
+import {Task} from "../model/Task";
+import {TestData} from "../data/TestData";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataHandlerService {
+
+    constructor() {
+    }
+
+    getCategories(): Category[] {
+        return TestData.categories;
+    }
+
+    getTasks(): Task[] {
+        return TestData.tasks;
+    }
+}
