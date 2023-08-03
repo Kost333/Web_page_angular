5660fa1ed7ae1852922785e4b9730020ce2457d2
Kost333 <levonkostanyan2021@gmail.com>
2023-08-03 13:45:24 +0400

    add categories and tasks by type of reactive extensions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CategoriesComponent} from './views/categories/categories.component';
-import {TasksComponent} from './views/tasks/tasks.component';
-import {AppRouterModule} from "./app-router/app-router.module";
+import {TasksComponent} from "./views/tasks/tasks.component";
 
 @NgModule({
     declarations: [
@@ -14,9 +12,7 @@ import {AppRouterModule} from "./app-router/app-router.module";
         TasksComponent
     ],
     imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        AppRouterModule
+        BrowserModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,27 +2,30 @@ import {Injectable} from '@angular/core';
 import {Category} from "../model/Category";
 import {Task} from "../model/Task";
 import {TestData} from "../data/TestData";
-import {Subject} from "rxjs";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
+
 export class DataHandlerService {
 
-    tasksSubject = new Subject<Task[]>();
+    tasksSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+    categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories);
 
     constructor() {
+        this.fillTasks();
     }
 
-    getCategories(): Category[] {
-        return TestData.categories;
-    }
+    // fillCategories() {
+    //     return TestData.categories
+    // }
 
-    fillTasks() {
+    fillTasks(): void {
         this.tasksSubject.next(TestData.tasks);
     }
 
-    fillTasksByCategory(category: Category) {
+    fillTasksByCategory(category: Category): void {
         const tasks = TestData.tasks.filter(task => task.category === category);
         this.tasksSubject.next(tasks);
     }
